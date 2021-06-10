f98066a6ba7daee935c41f93fb310dbcdf091dd8
huybn5776 <>
2021-06-10 12:05:57 +0800

    feat: show more description about loading state

--- src/components/VideoCaptionStateView.svelte
+++ src/components/VideoCaptionStateView.svelte
@@ -1,22 +1,36 @@
 <script lang="ts">
   import { VideoCaptionState } from '../enums/video-caption-state';
 
-  const wordings: Partial<Record<VideoCaptionState, string>> = {
+  const titles = {
+    null: 'This extension only work on udemy site.',
     [VideoCaptionState.loading]: 'Loading...',
     [VideoCaptionState.ready]: 'Caption loaded.',
     [VideoCaptionState.noCaption]: 'No caption loaded',
   };
-
+  const descriptions = {
+    null: 'Please open/switch to udemy tab and try again.',
+    [VideoCaptionState.loading]: 'fetching lecture data and captions.',
+  };
   export let state: VideoCaptionState | null = null;
 
-  $: text = wordings[state] || '';
+  $: title = titles[state || null] || '';
+  $: description = descriptions[state || null] || '';
 </script>
 
-<p class="video-caption-state">{text}</p>
+<h3 class="state-title">{title}</h3>
+{#if description}
+  <p class="state-description">{description}</p>
+{/if}
 
 <style lang="scss">
-  .video-caption-state {
+  .state-title {
     margin: 0;
     font-size: 15px;
+  }
+
+  .state-description {
+    margin: 4px 0;
+    color: gray;
+    font-size: 14px;
   }
 </style>
